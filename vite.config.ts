dcd7c8651d8637fe1d7f8a69eb074c995d39d6dc
pikumist <174439048+pikumist@users.noreply.github.com>
2024-07-13 05:48:57 +0900

    chore(vite): Use polling for chakura ui recommended

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/puyomist',
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 });
