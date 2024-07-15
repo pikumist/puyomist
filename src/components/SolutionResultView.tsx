492f9e127b7569fd6d3fd22e31708dd59b3adbd8
pikumist <174439048+pikumist@users.noreply.github.com>
2024-07-15 10:43:23 +0900

    refactor: Use chakra ui in SolutionResultView

--- src/components/SolutionResultView.tsx
+++ src/components/SolutionResultView.tsx
@@ -1,5 +1,5 @@
-import { Progress } from '@chakra-ui/react';
-import type React from 'react';
+import { Box, Progress, Text } from '@chakra-ui/react';
+import React from 'react';
 import {
   ExplorationCategory,
   getExplorationCategoryDescription
@@ -19,72 +19,72 @@ type SolutionResultViewProps = {
   result: ExplorationResult | undefined;
 };
 
-/** 探索結果View */
-const SolutionResultView: React.FC<SolutionResultViewProps> = (props) => {
-  const { solving, result } = props;
+/** 探索結果ビュー */
+const SolutionResultView: React.FC<SolutionResultViewProps> = React.memo(
+  (props) => {
+    const { solving, result } = props;
 
-  return (
-    <>
-      <Progress
-        size="xs"
-        visibility={solving ? 'visible' : 'hidden'}
-        isIndeterminate={solving}
-      />
-      <hr />
-      {result ? (
-        <div>
-          <div>
-            探索対象:{' '}
-            {getExplorationCategoryDescription(
-              result?.explorationTarget.category
-            )}
-          </div>
-          <div>探索時間: {result?.elapsedTime} ms</div>
-          <div>候補数: {result?.candidatesNum}</div>
-          <div>
-            最適なぞり:{' '}
-            {result?.optimalSolution?.traceCoords
-              .map((c) => c.toCellAddr())
-              .join(',')}
-          </div>
-          <div>
-            <OptimalValue result={result} />
-            {Simulator.colorAttrs.map((attr) => {
-              const description = getPuyoAttributeName(attr);
-              return (
-                <div key={attr}>
-                  {description}:{' '}
+    return (
+      <Box mt="1">
+        <Progress
+          size="xs"
+          visibility={solving ? 'visible' : 'hidden'}
+          isIndeterminate={solving}
+        />
+        {result ? (
+          <Box>
+            <Text>
+              探索対象:{' '}
+              {getExplorationCategoryDescription(
+                result?.explorationTarget.category
+              )}
+            </Text>
+            <Text>探索時間: {result?.elapsedTime} ms</Text>
+            <Text>候補数: {result?.candidatesNum}</Text>
+            <Text>
+              最適なぞり:{' '}
+              {result?.optimalSolution?.traceCoords
+                .map((c) => c.toCellAddr())
+                .join(',')}
+            </Text>
+            <Text>
+              <OptimalValue
+                category={result?.explorationTarget?.category}
+                optimalValue={result?.optimalSolution?.value}
+              />
+              {Simulator.colorAttrs.map((attr) => (
+                <Text key={attr}>
+                  {getPuyoAttributeName(attr)}:{' '}
                   {result?.optimalSolution?.totalDamages[
                     attr as ColoredPuyoAttribute
                   ]?.toFixed(2)}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ) : (
-        ''
-      )}
-    </>
-  );
-};
+                </Text>
+              ))}
+            </Text>
+          </Box>
+        ) : (
+          ''
+        )}
+      </Box>
+    );
+  }
+);
 
-const OptimalValue: React.FC<{ result: ExplorationResult | undefined }> = (
-  props
-) => {
-  const { result } = props;
+const OptimalValue: React.FC<{
+  category: ExplorationCategory | undefined;
+  optimalValue: number | undefined;
+}> = (props) => {
+  const { category, optimalValue } = props;
 
-  switch (result?.explorationTarget.category) {
+  switch (category) {
     case ExplorationCategory.Damage:
-      return (
-        <div>対象のダメージ量: {result?.optimalSolution?.value.toFixed(2)}</div>
-      );
+      return <Text>対象のダメージ量: {optimalValue?.toFixed(2)}</Text>;
     case ExplorationCategory.SkillPuyoCount:
-      return <div>スキル溜めぷよ数: {result?.optimalSolution?.value}</div>;
+      return <Text>スキル溜めぷよ数: {optimalValue}</Text>;
     case ExplorationCategory.PuyotsukaiCount:
-      return <div>ぷよ使いカウント: {result?.optimalSolution?.value}</div>;
+      return <Text>ぷよ使いカウント: {optimalValue}</Text>;
     default:
-      return <div>最適値: </div>;
+      return <Text>最適値: </Text>;
   }
 };
 
