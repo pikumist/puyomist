514dfcc2679a9259a24b93d2a33c75321f164455
pikumist <174439048+pikumist@users.noreply.github.com>
2024-07-24 04:01:14 +0900

    test(solver-wasm): Test SolutionState

--- packages/solver-wasm/src/solution.rs
+++ packages/solver-wasm/src/solution.rs
@@ -26,6 +26,10 @@ impl SolutionState {
         }
     }
 
+    pub fn __get_forbidden_field_bits(&self) -> u64 {
+        return self.forbidden_field_bits;
+    }
+
     /** なぞり座標リストを取得する */
     pub fn get_trace_coords(&self) -> Vec<PuyoCoord> {
         return self.trace_coord_map.keys().cloned().collect();
@@ -158,4 +162,129 @@ pub struct ExplorationResult {
     pub candidates_num: u64,
     /** 最適解リスト。インデックスが小さい要素ほど最善 */
     pub optimal_solutions: Vec<SolutionResult>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_forbidden_field_bits() {
+        assert_eq!(SolutionState::new(0).__get_forbidden_field_bits(), 0);
+        assert_eq!(SolutionState::new(1).__get_forbidden_field_bits(), 1);
+        assert_eq!(SolutionState::new(2).__get_forbidden_field_bits(), 3);
+        assert_eq!(SolutionState::new(3).__get_forbidden_field_bits(), 7);
+        assert_eq!(
+            SolutionState::new(48).__get_forbidden_field_bits(),
+            281474976710655
+        );
+    }
+
+    #[test]
+    fn test_get_trace_coords() {
+        let mut s = SolutionState::new(0);
+        assert_eq!(s.get_trace_coords(), []);
+        s.add_trace_coord(PuyoCoord { x: 0, y: 0 });
+        assert_eq!(s.get_trace_coords(), [PuyoCoord { x: 0, y: 0 }]);
+        s.add_trace_coord(PuyoCoord { x: 1, y: 1 });
+        assert_eq!(
+            s.get_trace_coords(),
+            [PuyoCoord { x: 0, y: 0 }, PuyoCoord { x: 1, y: 1 }]
+        );
+    }
+
+    #[test]
+    fn test_check_if_addable_coord_around_1_1() {
+        // Arrange
+        let mut s = SolutionState::new(0);
+        s.add_trace_coord(PuyoCoord { x: 1, y: 1 });
+
+        // Act & Assert
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 0, y: 0 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 1, y: 0 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 2, y: 0 }, 5), true);
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 3, y: 0 }, 5),
+            false
+        );
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 0, y: 1 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 2, y: 1 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 0, y: 2 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 1, y: 2 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 2, y: 2 }, 5), true);
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 1, y: 3 }, 5),
+            false
+        );
+    }
+
+    #[test]
+    fn test_check_if_addable_coord_by_max() {
+        // Arrange
+        let mut s = SolutionState::new(0);
+        s.add_trace_coord(PuyoCoord { x: 1, y: 1 });
+        let c = PuyoCoord { x: 0, y: 0 };
+
+        // Act & Assert
+        assert_eq!(s.check_if_addable_coord(&c, 1), false);
+        assert_eq!(s.check_if_addable_coord(&c, 2), true);
+    }
+
+    #[test]
+    fn test_check_if_addable_coord_by_forbidden() {
+        // Arrange
+        let mut s = SolutionState::new(9);
+        s.add_trace_coord(PuyoCoord { x: 1, y: 1 });
+
+        // Act & Assert
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 0, y: 0 }, 5),
+            false
+        );
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 0, y: 1 }, 5),
+            false
+        );
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 2, y: 1 }, 5), true);
+    }
+
+    #[test]
+    fn test_addable_and_forbidden() {
+        // Arrange
+        let mut s = SolutionState::new(9);
+        s.add_trace_coord(PuyoCoord { x: 1, y: 1 });
+        s.add_trace_coord(PuyoCoord { x: 2, y: 2 });
+
+        // Assert: 禁止集合に含まれているか候補集合に含まれていないものはaddableでない
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 1, y: 2 }, 5),
+            false
+        );
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 2, y: 1 }, 5),
+            false
+        );
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 3, y: 2 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 1, y: 3 }, 5), true);
+        assert_eq!(s.check_if_addable_coord(&PuyoCoord { x: 2, y: 3 }, 5), true);
+        assert_eq!(
+            s.check_if_addable_coord(&PuyoCoord { x: 2, y: 4 }, 5),
+            false
+        );
+        let expected: IndexMap<PuyoCoord, IndexSet<PuyoCoord>> = IndexMap::from([
+            (PuyoCoord { x: 1, y: 1 }, IndexSet::from([])),
+            (
+                PuyoCoord { x: 2, y: 2 },
+                IndexSet::from([
+                    PuyoCoord { x: 3, y: 1 },
+                    PuyoCoord { x: 3, y: 2 },
+                    PuyoCoord { x: 1, y: 3 },
+                    PuyoCoord { x: 2, y: 3 },
+                    PuyoCoord { x: 3, y: 3 },
+                ]),
+            ),
+        ]);
+        assert_eq!(*s.get_trace_coord_map(), expected);
+        assert_eq!(s.__get_forbidden_field_bits(), 460799);
+    }
 }
