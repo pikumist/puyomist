d548806549b6497c7e63247a133c4f3f7c35a717
pikumist <174439048+pikumist@users.noreply.github.com>
2025-01-07 22:24:19 +0900

    feat: Add 塗り替え options in board edit (#78)

--- src/logics/BoardEditMode.ts
+++ src/logics/BoardEditMode.ts
@@ -20,8 +20,23 @@ export enum HowToEditBoard {
   /** 色ぷよであればプラスを付与する。 */
   AddPlus = 3,
 
+  /** 赤ぷよに塗り替えする。 */
+  ToRed = 4,
+
+  /** 青ぷよに塗り替えする。 */
+  ToBlue = 5,
+
+  /** 緑ぷよに塗り替えする。 */
+  ToGreen = 6,
+
+  /** 黄ぷよに塗り替えする。 */
+  ToYellow = 7,
+
+  /** 紫ぷよに塗り替えする。 */
+  ToPurple = 8,
+
   /** 任意のぷよに変換する。 */
-  ToCustomType = 4
+  ToCustomType = 9
 }
 
 /** ボード編集の仕方と説明のマップ */
@@ -30,5 +45,10 @@ export const howToEditBoardDescriptionMap: ReadonlyMap<HowToEditBoard, string> =
     [HowToEditBoard.ClearEnhance, '色ぷよの付与を消去'],
     [HowToEditBoard.AddChance, 'チャンスを付与'],
     [HowToEditBoard.AddPlus, 'プラスを付与'],
+    [HowToEditBoard.ToRed, '赤ぷよに塗り替え'],
+    [HowToEditBoard.ToBlue, '青ぷよに塗り替え'],
+    [HowToEditBoard.ToGreen, '緑ぷよに塗り替え'],
+    [HowToEditBoard.ToYellow, '黄ぷよに塗り替え'],
+    [HowToEditBoard.ToPurple, '紫ぷよに塗り替え'],
     [HowToEditBoard.ToCustomType, '任意のぷよに変換']
   ]);
